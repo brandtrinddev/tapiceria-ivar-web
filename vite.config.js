848f968d2a8895d7d7e9249b2008e761cf8683c9
brandtrinddev <brandtrind.dev@gmail.com>
2025-08-16 16:04:14 -0300

    Configurar generación automática del Sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap' // <--- 1. AÑADE ESTA LÍNEA
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: 'all'
-  }
+  plugins: [
+    react(),
+    sitemap({ hostname: 'https://www.tapiceriaivar.com.uy' }) // <--- 2. AÑADE ESTA LÍNEA
+  ],
 })
