ca225192d781815be2dd3751e80d991d72a16be3
brandtrinddev <brandtrind.dev@gmail.com>
2025-08-16 16:22:33 -0300

    Fix: bugs del CSS nav y sitemap incompleto

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap' // <--- 1. AÑADE ESTA LÍNEA
+import sitemap from 'vite-plugin-sitemap'
+import productData from './src/data/productos.json' // <-- 1. IMPORTAMOS LOS DATOS
+
+// 2. CREAMOS LA LISTA DE RUTAS DE PRODUCTOS
+const dynamicRoutes = productData.map(product => `/producto/${product.id}`)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({ hostname: 'https://www.tapiceriaivar.com.uy' }) // <--- 2. AÑADE ESTA LÍNEA
+    sitemap({
+      hostname: 'https://www.tapiceriaivar.com.uy',
+      // 3. AÑADIMOS LAS RUTAS DINÁMICAS A LA CONFIGURACIÓN
+      dynamicRoutes: dynamicRoutes
+    })
   ],
 })
